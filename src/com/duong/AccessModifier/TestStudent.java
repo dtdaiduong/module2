176d5239f115685f9ccd34e3a0cf43607da7d58b
dtdaiduong <dotrongdaiduong@gmail.com>
2021-12-14 16:24:11 +0700

    ngay 14/12

---
+++ src/com/duong/AccessModifier/TestStudent.java
@@ -0,0 +1,13 @@
+package com.duong.AccessModifier;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.getNames());
+        System.out.println(student.getClasses());
+        student.setNames("dan");
+        student.setClasses("C1021");
+        System.out.println(student.getNames());
+        System.out.println(student.getClasses());
+    }
+}
